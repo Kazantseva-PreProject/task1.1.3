01a9ef0396ca5da529a20fd04831774067819697
Ekaterina-Kazantsev <v_tin@mail.ru>
2023-10-12 10:49:17 +0300

    Задача 1.1.3 готова к проверке

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 20);
+        userService.saveUser("Petr", "Petrov", (byte) 25);
+        userService.saveUser("Sidr", "Sidorov", (byte) 30);
+        userService.saveUser("Aleksandr", "Aleksandrov", (byte) 35);
+
+        List<User> users = userService.getAllUsers();
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
